7259e5d733f0eff101fbab83e5d7d4bc770c5def
Geetha <danda.saigeetha@wipro.com>
2024-04-21 18:45:34 +0000

    added file

---
+++ src/main/java/javaprg/wellcome_code.java
@@ -0,0 +1,15 @@
+package javaprg;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class wellcome_code {
+
+	public static void main(String[] args) {
+		String string = "hello wellcome";
+   
+        System.out.println("enter the string vale "+StringUtils.length(string));
+   
+
+	}
+
+}
